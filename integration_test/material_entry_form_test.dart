c291b470e75fdfd155f592ad9ee46680849a9a8b
duylequang2303 <duylequang588@gmail.com>
2026-07-25 21:12:40 +0700

    test(e2e): inject explicit mock data for material entry form test

--- integration_test/material_entry_form_test.dart
+++ integration_test/material_entry_form_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
 import 'package:fieldforce_mobile/widgets/material_entry_form.dart';
+import 'package:fieldforce_mobile/features/stock/models/product.dart';
 
 void main() {
   patrolTest(
@@ -15,6 +16,10 @@ void main() {
         MaterialApp(
           home: Scaffold(
             body: MaterialEntryForm(
+              availableProducts: [
+                Product()..name = 'AC Filter (Standard)'..standardPrice = 10.0,
+                Product()..name = 'Oil Filter'..standardPrice = 5.0,
+              ],
               onSaved: () {
                 isSaved = true;
               },
